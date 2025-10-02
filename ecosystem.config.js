78f0e1a2744a8d39be37969bc4c13b661cc9ed21
abhi02max <abhideep0204@gmail.com>
2025-10-02 15:46:55 +0530

    Going On

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       PORT: 5001,
-      MONGO_URI: 'mongodb+srv://admin:Password123@spacluster.tkhog8t.mongodb.net/moneri?retryWrites=true&w=majority&appName=SpaCluster',
+      MONGO_URI: 'mongodb+srv://admin:password123456.@spacluster.tkhog8t.mongodb.net/moneri?retryWrites=true&w=majority&appName=SpaCluster',
       JWT_SECRET: 'A_STRONG_SECRET_KEY_HERE',
       EMAIL_USER: 'info@monerispaacademy.in',
       EMAIL_PASS: 'Info23456.',
@@ -19,7 +19,7 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production',
       PORT: 5001,
-      MONGO_URI: 'mongodb+srv://admin:Password123@spacluster.tkhog8t.mongodb.net/moneri?retryWrites=true&w=majority&appName=SpaCluster',
+      MONGO_URI: 'mongodb+srv://admin:password123456.@spacluster.tkhog8t.mongodb.net/moneri?retryWrites=true&w=majority&appName=SpaCluster',
       JWT_SECRET: 'A_STRONG_SECRET_KEY_HERE',
       EMAIL_USER: 'info@monerispaacademy.in',
       EMAIL_PASS: 'Info23456.',
