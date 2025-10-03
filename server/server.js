844afb19480c52d13fb687468319530578de70e3
abhi02max <abhideep0204@gmail.com>
2025-10-03 23:13:47 +0530

    Fix path-to-regexp error: Use app.use() instead of app.get('*') for catch-all route

--- server/server.js
+++ server/server.js
@@ -141,7 +141,7 @@ if (process.env.NODE_ENV === 'production') {
     etag: true
   }));
   
-  app.get('/*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
